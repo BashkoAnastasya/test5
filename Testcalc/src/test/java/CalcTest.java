1c5897dca71fd43de26774e667ae5d28560f8dbb
BashkoAnastasya <nastinm@mail.ru>
2018-06-13 14:19:07 +0300

    allure

--- Testcalc/src/test/java/CalcTest.java
+++ Testcalc/src/test/java/CalcTest.java
@@ -18,112 +18,116 @@ public class CalcTest {
         classCalc = null;
     }
 
-    @Test
-    public void testGetSum1() throws Exception {
-        System.out.println("testGetSum1()");
-        assertEquals(4, classCalc.getSum(2, 2), 0.01);
-    }
 
     @Test
-    public void testGetSum2() throws Exception {
-        System.out.println("testGetSum2()");
+    public void GetSumPosPosDouble() throws Exception {
+        System.out.println("Pos+Pos");
         assertEquals(4.9, classCalc.getSum(2.3, 2.6), 0.01);
     }
 
     @Test
-    public void testGetSum3() throws Exception {
-        System.out.println("testGetSum3()");
+    public void GetSumNegNegInt() throws Exception {
+        System.out.println("Neg+Neg");
         assertEquals(-11, classCalc.getSum(-5, -6), 0.01);
     }
 
     @Test
-    public void testGetSum4() throws Exception {
-        System.out.println("testGetSum4()");
+    public void GetSumPosNull() throws Exception {
+        System.out.println("Pos+0");
         assertEquals(2, classCalc.getSum(2, 0), 0.01);
     }
 
-    @Test
-    public void testGetSum5() throws Exception {
-        System.out.println("testGetSum5()");
-        assertEquals(1000, classCalc.getSum(999, 1), 0.01);
-    }
 
     @Test
-    public void testGetMultiplication6() throws Exception {
-        System.out.println("testGetUmn6()");
-        assertEquals(4, classCalc.getMultiplication(2, 2), 0.01);
-    }
-
-    @Test
-    public void testGetMultiplication7() throws Exception {
-        System.out.println("testGetUmn7()");
+    public void testGetMultiplicationPosPos() throws Exception {
+        System.out.println("Pos*Pos");
         assertEquals(4.84, classCalc.getMultiplication(2.2, 2.2), 0.01);
     }
 
     @Test
-    public void testGetMultiplication8() throws Exception {
-        System.out.println("testGetUmn8()");
+    public void testGetMultiplicationPosNeg() throws Exception {
+        System.out.println("Pos*Neg");
+        assertEquals(-4.84, classCalc.getMultiplication(2.2, -2.2), 0.01);
+    }
+
+    @Test
+    public void testGetMultiplicationNegNeg() throws Exception {
+        System.out.println("Neg*Neg");
+        assertEquals(4.84, classCalc.getMultiplication(-2.2, -2.2), 0.01);
+    }
+
+    @Test
+    public void testGetMultiplicationPosNull() throws Exception {
+        System.out.println("Pos*Null");
         assertEquals(0, classCalc.getMultiplication(2, 0), 0.01);
     }
 
     @Test
-    public void testGetMinus9() throws Exception {
-        System.out.println("testGetMinus9()");
+    public void testGetMinusPosPos() throws Exception {
+        System.out.println("Pos-Pos");
         assertEquals(0, classCalc.getMinus(2, 2), 0.01);
     }
 
     @Test
-    public void testGetMinus10() throws Exception {
-        System.out.println("testGetMinus10()");
-        assertEquals(-0.2, classCalc.getMinus(-2.3, -2.1), 0.01);
+    public void testGetMinusPosNeg() throws Exception {
+        System.out.println("Pos-Neg");
+        assertEquals(4.4, classCalc.getMinus(2.3, -2.1), 0.01);
     }
 
     @Test
-    public void testGetDivision11() throws Exception {
-        System.out.println("testGetDivision11()");
+    public void testGetMinusNegNeg() throws Exception {
+        System.out.println("Neg-Neg");
+        assertEquals(-0.2, classCalc.getMinus(-2.3, -2.1), 0.01);
+    }
+
+
+    @Test
+    public void testGetDivisionPosPos() throws Exception {
+        System.out.println("Pos/Pos");
         assertEquals(1, classCalc.getDivision(2, 2), 0.01);
     }
 
     @Test
-    public void testGetDivision12() throws Exception {
-        System.out.println("testGetDivision12()");
+    public void testGetDivisionNegNeg() throws Exception {
+        System.out.println("Neg/Neg");
         assertEquals(1.095, classCalc.getDivision(-2.3, -2.1), 0.01);
     }
 
     @Test
-    public void testGetDivision3() throws Exception {
-        System.out.println("testGetDivision3()");
-        assertEquals(1.095, classCalc.getDivision(2.3, 2.1), 0.01);
+    public void testGetDivisionPosNeg() throws Exception {
+        System.out.println("Neg/Neg");
+        assertEquals(-1.095, classCalc.getDivision(2.3, -2.1), 0.01);
+    }
+
+
+    @Test
+    public void testOperationPlus() {
+        assertEquals("+", 7, classCalc.calculator(3, 4, '+'), 0.01);
     }
 
     @Test
-    public void testOperation14() {
-        assertEquals("add", 7, classCalc.calculator(3, 4, '+'), 0.01);
+    public void testOperationMultiplication() {
+        assertEquals("*", 12, classCalc.calculator(3, 4, '*'), 0.01);
     }
 
     @Test
-    public void testOperation15() {
-        assertEquals("add", 12, classCalc.calculator(3, 4, '*'), 0.01);
+    public void testOperationMinus() {
+        assertEquals("-", -1, classCalc.calculator(3, 4, '-'), 0.01);
     }
 
     @Test
-    public void testOperation16() {
-        assertEquals("add", -1, classCalc.calculator(3, 4, '-'), 0.01);
-    }
-
-    @Test
-    public void testOperation17() {
+    public void testOperationDivision() {
         assertTrue(classCalc.calculator(3, 1, '/') == 3);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testOperation18() {
-        assertEquals("add", 1, classCalc.calculator(3.3, 0, '/'), 0.01);
+    public void testOperationDivOnNull() {
+        assertEquals("/null", 1, classCalc.calculator(3.3, 0, '/'), 0.01);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testOperation19() {
-        assertEquals("add", 1, classCalc.calculator(3.3, 0, '1'), 0.01);
+    public void testOperationNo() {
+        assertEquals("not operation", 1, classCalc.calculator(3.3, 0, '1'), 0.01);
 
 
     }
